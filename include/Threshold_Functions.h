96678e3d4bd1653dee8f6d64897b141f71ad99a7
David Emerson <davemers@umail.iu.edu>
2018-02-09 09:39:05 -0500

    added a third ranged threshold function and simple example code

--- include/Threshold_Functions.h
+++ include/Threshold_Functions.h
@@ -7,7 +7,8 @@
 #include <opencv2/highgui/highgui.hpp>     
 #include <opencv2/imgproc/imgproc.hpp>  
 
-void advancedThreshold(Mat &inputImage, int threshold, int maxval, int minval);
+void advancedThreshold(cv::Mat inputImage, cv::Mat &thresholdImage, int threshold, int min_val, int max_val);
+void rangedThreshold(cv::Mat inputImage, cv::Mat &thresholdImage, int min_val, int max_val);
 void energyThreshold(cv::Mat inputImage, double energyVal, cv::Mat &thresholdImage, int &threshVal, int method);
 
 #endif	// end of THRESHOLD_FUNCTIONS_H
--- src/Threshold_Functions.cpp
+++ src/Threshold_Functions.cpp
@@ -23,34 +23,60 @@
 using namespace std;
 using namespace cv;
 
-void advancedThreshold(cv::Mat &inputImage, int threshold, int maxval, int minval)
+void advancedThreshold(cv::Mat inputImage, cv::Mat &thresholdImage, int threshold, int min_val, int max_val)
 {
-	// accept only char type matrices
+	// accept only single channel char type matrices
 	//CV_Assert(inputImage.depth() == CV_8U);
 
-	int channels = inputImage.channels();
+	//int channels = inputImage.channels();
 
-	cv::MatIterator_<short> it, end;
-	for (it = inputImage.begin<short>(), end = inputImage.end<short>(); it != end; ++it)
+	cv::MatIterator_<unsigned char> it, end;
+    cv::MatIterator_<unsigned char> thresh_it = thresholdImage.begin<unsigned char>();
+
+	for (it = inputImage.begin<unsigned char>(), end = inputImage.end<unsigned char>(); it != end; ++it, ++thresh_it)
 	{
-		//*it = table[*it];
 		if (*it == threshold)
 		{
-			*it = maxval;
+			*thresh_it = max_val;
 		}
 		else
 		{
-			*it = minval;
+			*thresh_it = min_val;
 		}
 	}
 
 }	// end of advancedThreshold
 
+// threshold function that will zero out everything that is not in the range [minval, maxval]
+void rangedThreshold(cv::Mat inputImage, cv::Mat &thresholdImage, int min_val, int max_val)
+{
+    //CV_Assert(inputImage.depth() == CV_8U);
+
+    cv::MatIterator_<unsigned char> it, end;
+    cv::MatIterator_<unsigned char> thresh_it = thresholdImage.begin<unsigned char>();
+    for (it = inputImage.begin<unsigned char>(), end = inputImage.end<unsigned char>(); it != end; ++it, ++thresh_it)
+    {
+        if (*it < min_val || *it > max_val)
+        {
+            *thresh_it = 0;
+        }
+        else
+        {
+            *thresh_it = *it;
+        }
+    }
+
+
+
+}   // end of rangedThreshold
+
 // attempt to threshold the image by looking at the total energy value and reducing it to less
 // than energyVal
 void energyThreshold(cv::Mat inputImage, double energyVal, cv::Mat &thresholdImage, int &threshVal, int method)
 {
-	int idx;
+    //CV_Assert(inputImage.depth() == CV_8U);
+
+    int idx;
 	//double totalEnergy = (double)cv::sum(inputImage)[0];
 	double intialEnergy = (double)cv::sum(inputImage)[0];
 	double currentEnergy = 0.0;
@@ -110,7 +136,7 @@ void energyThreshold(cv::Mat inputImage, double energyVal, cv::Mat &thresholdIma
 	//cv::erode(thresholdImage, thresholdImage, cv::Mat(cv::Size(3, 3), CV_8UC1, cv::Scalar(1)), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::Scalar(0));
 	//cv::dilate(thresholdImage, thresholdImage, cv::Mat(cv::Size(3, 3), CV_8UC1, cv::Scalar(1)), cv::Point(-1, -1), 2, BORDER_CONSTANT, cv::Scalar(0));
 	//cv::erode(thresholdImage, thresholdImage, cv::Mat(cv::Size(3, 3), CV_8UC1, cv::Scalar(1)), cv::Point(-1, -1), 2, BORDER_CONSTANT, cv::Scalar(0));
-	cv::dilate(thresholdImage, thresholdImage, cv::Mat(cv::Size(3, 3), CV_8UC1, cv::Scalar(1)), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::Scalar(0));
+	//cv::dilate(thresholdImage, thresholdImage, cv::Mat(cv::Size(3, 3), CV_8UC1, cv::Scalar(1)), cv::Point(-1, -1), 1, BORDER_CONSTANT, cv::Scalar(0));
 
 }	// end of energyThreshold
 
